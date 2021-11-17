0c8edc8c79ccc77c02643f563cd37652a704bbee
tashinam <tashinamgyal@mac.com>
2021-11-17 22:53:51 +0000

    add utils

--- util.py
+++ util.py
@@ -3,6 +3,8 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import torch
 import torch.nn.functional as F
+import numpy as np
+
 
 class Maestro(Dataset):
 
@@ -22,3 +24,23 @@ class Maestro(Dataset):
         # coh = F.one_hot(c, cond_height).type(torch.FloatTensor)
         # return (doh, coh)
         return doh
+
+
+def note2scale(x):
+    n2s = {'0': 60, '1': 62, '2': 64, '3': 65, '4': 67, '5': 69, '6': 71,
+           '7': 72, '8': 74, '9': 76, '10': 77, '11': 79, '12': 81, '13': 83,
+           '14': 84}
+    return n2s.get(str(int(x)), x)
+
+
+def scale2note(x):
+    s2n = {'60': 0, '62': 1, '64': 2, '65': 3, '67': 4, '69': 5, '71': 6,
+           '72': 7, '74': 8, '76': 9, '77': 10, '79': 11, '81': 12, '83': 13,
+           '84': 14}
+    return s2n.get(str(int(x)), x)
+
+
+def best_fit_slope_and_intercept(xs, ys):
+    m = (((np.mean(xs) * np.mean(ys)) - np.mean(xs * ys)) / ((np.mean(xs) * np.mean(xs)) - np.mean(xs * xs)))
+    b = np.mean(ys) - m * np.mean(xs)
+    return m, b
